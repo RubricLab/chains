2d18a363aca753541c0559b8968b2304cb63a229
DexterStorey <dexter@dexterstorey.com>
2025-05-30 18:25:09 -0400

    cleanup

---
+++ lib/chains.ts
@@ -0,0 +1,228 @@
+import { z } from 'zod/v4'
+import type { $strict } from 'zod/v4/core'
+
+type SupportedZodTypes =
+	| z.ZodString
+	| z.ZodNumber
+	| z.ZodBoolean
+	| z.ZodUndefined
+	| z.ZodVoid
+	| z.ZodObject<Record<string, SupportedZodTypes>>
+	| z.ZodArray<SupportedZodTypes>
+
+type Node = {
+	input: Record<string, SupportedZodTypes>
+	output: SupportedZodTypes
+}
+
+type ShapeOf<T extends SupportedZodTypes> = T extends z.ZodString
+	? 'string'
+	: T extends z.ZodNumber
+		? 'number'
+		: T extends z.ZodBoolean
+			? 'boolean'
+			: T extends z.ZodUndefined
+				? 'undefined'
+				: T extends z.ZodVoid
+					? 'void'
+					: T extends z.ZodObject<infer InnerShape extends Record<string, SupportedZodTypes>>
+						? { [K in keyof InnerShape]: ShapeOf<InnerShape[K]> }
+						: T extends z.ZodArray<infer InnerShape extends SupportedZodTypes>
+							? ShapeOf<InnerShape>[]
+							: never
+
+function shapeOf<Shape extends SupportedZodTypes>(shape: Shape): ShapeOf<SupportedZodTypes> {
+	switch (shape.def.type) {
+		case 'object': {
+			return Object.fromEntries(
+				Object.entries(shape.def.shape).map(([key, value]) => {
+					return [key, shapeOf(value)]
+				})
+			) as ShapeOf<Shape>
+		}
+		case 'array': {
+			return [shapeOf(shape.def.element)] as ShapeOf<Shape>
+		}
+		case 'number': {
+			return 'number' as ShapeOf<Shape>
+		}
+		case 'string': {
+			return 'string' as ShapeOf<Shape>
+		}
+		case 'boolean': {
+			return 'boolean' as ShapeOf<Shape>
+		}
+		case 'undefined': {
+			return 'undefined' as ShapeOf<Shape>
+		}
+		case 'void': {
+			return 'void' as ShapeOf<Shape>
+		}
+	}
+}
+
+type Compatable<
+	Type1 extends SupportedZodTypes,
+	Type2 extends SupportedZodTypes
+> = ShapeOf<Type1> extends ShapeOf<Type2> ? true : false
+
+type NodeCompatability<
+	Type extends SupportedZodTypes,
+	Nodes extends Record<string, Node>,
+	Strict extends boolean
+> = {
+	[K in keyof Nodes]: Compatable<Nodes[K]['output'], Type> extends true
+		? NodeDefinition<K & string, Nodes[K]['input'], Nodes, Strict>
+		: never
+}[keyof Nodes][]
+
+type NodeDefinition<
+	Name extends string,
+	Input extends Record<string, SupportedZodTypes>,
+	Nodes extends Record<string, Node>,
+	Strict extends boolean
+> = z.ZodObject<
+	{
+		node: z.ZodLiteral<Name>
+		input: z.ZodObject<
+			{
+				[K in keyof Input]: Strict extends true
+					? z.ZodUnion<NodeCompatability<Input[K], Nodes, Strict>>
+					: z.ZodUnion<[Input[K], ...NodeCompatability<Input[K], Nodes, Strict>]>
+			},
+			$strict
+		>
+	},
+	$strict
+>
+
+type Definition<
+	Name extends string = string,
+	Input extends Record<string, SupportedZodTypes> = Record<string, SupportedZodTypes>,
+	Nodes extends Record<string, Node> = Record<string, Node>,
+	Strict extends boolean = boolean
+> = {
+	definition: NodeDefinition<Name, Input, Nodes, Strict>
+	output: SupportedZodTypes
+}
+
+function getNodeCompatabilities<
+	Type extends SupportedZodTypes,
+	Definitions extends Record<string, Definition>
+>({ type, definitions }: { type: Type; definitions: Definitions }) {
+	return Object.values(definitions)
+		.filter(({ output }) => {
+			return JSON.stringify(shapeOf(output)) === JSON.stringify(shapeOf(type))
+		})
+		.map(({ definition }) => definition)
+}
+
+export function createChain<Nodes extends Record<string, Node>, Strict extends boolean = false>(
+	nodes: Nodes,
+	{ strict }: { strict: Strict } = { strict: false as Strict }
+) {
+	function createNodeDefinition<
+		Name extends string,
+		Input extends Record<string, SupportedZodTypes>
+	>({
+		name,
+		input
+	}: {
+		name: Name
+		input: Input
+	}) {
+		return z.object({
+			node: z.literal(name),
+			input: z.object(
+				Object.defineProperties(
+					{},
+					Object.fromEntries(
+						Object.keys(input).map(key => [
+							key,
+							{
+								enumerable: true,
+								configurable: false,
+								get() {
+									return compatabilities[name]?.[key]?.schema
+								}
+							}
+						])
+					)
+				)
+			)
+		})
+	}
+
+	// Internal, recursive
+	const definitions = Object.fromEntries(
+		Object.entries(nodes).map(([name, node]) => [
+			name,
+			{
+				definition: createNodeDefinition({ name, input: node.input }),
+				output: node.output
+			}
+		])
+	) as {
+		[K in keyof Nodes]: Definition<K & string, Nodes[K]['input'], Nodes, Strict>
+	}
+
+	// Internal, recursive
+	const compatabilities = Object.fromEntries(
+		Object.entries(nodes).map(([name, node]) => {
+			return [
+				name,
+				Object.fromEntries(
+					Object.entries(node.input).map(([key, arg]) => {
+						return [
+							key,
+							{
+								shape: shapeOf(arg),
+								schema: strict
+									? z.union([...getNodeCompatabilities({ type: arg, definitions })])
+									: z.union([arg, ...getNodeCompatabilities({ type: arg, definitions })])
+							}
+						]
+					})
+				)
+			]
+		})
+	) as {
+		[K in keyof Nodes]: {
+			[I in keyof Nodes[K]['input']]: {
+				shape: ShapeOf<Nodes[K]['input'][I]>
+				schema: Strict extends true
+					? z.ZodUnion<NodeCompatability<Nodes[K]['input'][I], Nodes, Strict>>
+					: z.ZodUnion<[Nodes[K]['input'][I], ...NodeCompatability<Nodes[K]['input'][I], Nodes, Strict>]>
+			}
+		}
+	}
+
+	type Definitions = typeof definitions
+
+	type Compatabilities = typeof compatabilities
+
+	const __definitions = Object.values(definitions).map(({ definition }) => definition) as {
+		[K in keyof Definitions]: Definitions[K]['definition']
+	}[keyof Definitions][]
+
+	const __compatabilities = Object.values(compatabilities)
+		.flatMap(Object.values)
+		.reduce((acc, obj) => {
+			if (!acc.some((item: Compatabilities[keyof Compatabilities]) => item.shape === obj.shape)) {
+				acc.push(obj)
+			}
+			return acc
+		}, []) as {
+		[K in keyof Compatabilities]: {
+			[J in keyof Compatabilities[K]]: {
+				shape: Compatabilities[K][J]['shape']
+				schema: Compatabilities[K][J]['schema']
+			}
+		}[keyof Compatabilities[K]]
+	}[keyof Compatabilities][]
+
+	return {
+		definitions: __definitions,
+		compatabilities: __compatabilities
+	}
+}
