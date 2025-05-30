02f2a364ca0afc0860ed488b1e17d535d0117edf
DexterStorey <dexter@dexterstorey.com>
2025-05-30 17:08:55 -0400

    patch circular dep issue!! no hacks!!

--- lib/index.ts
+++ lib/index.ts
@@ -10,7 +10,7 @@ type SupportedZodTypes =
 	| z.ZodObject<Record<string, SupportedZodTypes>>
 	| z.ZodArray<SupportedZodTypes>
 
-type GenericNode = {
+type Node = {
 	input: Record<string, SupportedZodTypes>
 	output: SupportedZodTypes
 }
@@ -61,87 +61,95 @@ function shapeOf<Shape extends SupportedZodTypes>(shape: Shape): ShapeOf<Support
 	}
 }
 
-export function createChain<Nodes extends Record<string, GenericNode>>(nodes: Nodes) {
-	// @ts-expect-error circular dependency hack
-	type Definition<CIRCUIT_BREAKER extends never> = {
-		definition: ReturnType<typeof createDefinitions>
-		output: SupportedZodTypes
-	}
+type Compatable<
+	Type1 extends SupportedZodTypes,
+	Type2 extends SupportedZodTypes
+> = ShapeOf<Type1> extends ShapeOf<Type2> ? true : false
 
-	function compatableWith<
-		Type extends SupportedZodTypes,
-		// @ts-expect-error circular dependency hack
-		Definitions extends Record<string, Definition>
-	>({ shapeName, type, definitions }: { shapeName: string; type: Type; definitions: Definitions }) {
-		return Object.entries(definitions)
-			.filter(([defName, { output }]) => {
-				console.log('COMPARING', {
-					[defName]: JSON.stringify(shapeOf(output)),
-					[shapeName]: JSON.stringify(shapeOf(type)),
-					equal: JSON.stringify(shapeOf(output)) === JSON.stringify(shapeOf(type)),
-					shapeOf: shapeOf(output) === shapeOf(type)
-				})
-				return JSON.stringify(shapeOf(output)) === JSON.stringify(shapeOf(type))
-			})
-			.map(([_defName, { definition }]) => definition) as {
-			[K in keyof Definitions]: ShapeOf<Definitions[K]['output']> extends ShapeOf<Type>
-				? Definitions[K]['definition']
-				: never
-		}[keyof Definitions][]
-	}
+type NodeCompatability<Type extends SupportedZodTypes, Nodes extends Record<string, Node>> = {
+	[K in keyof Nodes]: Compatable<Nodes[K]['output'], Type> extends true
+		? NodeDefinition<K & string, Nodes[K]['input'], Nodes>
+		: never
+}[keyof Nodes][]
 
-	function createDefinitions<Name extends string, Input extends Record<string, SupportedZodTypes>>({
+type NodeDefinition<
+	Name extends string,
+	Input extends Record<string, SupportedZodTypes>,
+	Nodes extends Record<string, Node>
+> = z.ZodObject<
+	{
+		node: z.ZodLiteral<Name>
+		input: z.ZodObject<
+			{ [K in keyof Input]: z.ZodUnion<NodeCompatability<Input[K], Nodes>> },
+			$strict
+		>
+	},
+	$strict
+>
+
+type Definition<
+	Name extends string = string,
+	Input extends Record<string, SupportedZodTypes> = Record<string, SupportedZodTypes>,
+	Nodes extends Record<string, Node> = Record<string, Node>
+> = {
+	definition: NodeDefinition<Name, Input, Nodes>
+	output: SupportedZodTypes
+}
+
+function getNodeCompatabilities<
+	Type extends SupportedZodTypes,
+	Definitions extends Record<string, Definition>
+>({ type, definitions }: { type: Type; definitions: Definitions }) {
+	return Object.values(definitions)
+		.filter(({ output }) => {
+			return JSON.stringify(shapeOf(output)) === JSON.stringify(shapeOf(type))
+		})
+		.map(({ definition }) => definition)
+}
+
+export function createChain<Nodes extends Record<string, Node>>(nodes: Nodes) {
+	function createNodeDefinition<
+		Name extends string,
+		Input extends Record<string, SupportedZodTypes>
+	>({
 		name,
 		input
-	}: { name: Name; input: Input }) {
+	}: {
+		name: Name
+		input: Input
+	}) {
 		return z.object({
-			node: z.literal(name as Name extends string ? Name : never),
+			node: z.literal(name),
 			input: z.object(
 				Object.defineProperties(
 					{},
 					Object.fromEntries(
-						Object.entries(input).map(([key, arg]) => [
+						Object.keys(input).map(key => [
 							key,
 							{
 								enumerable: true,
 								configurable: false,
 								get() {
-									return compatabilities[name][key]?.zod
+									return compatabilities[name]?.[key]?.zod
 								}
 							}
 						])
 					)
 				)
 			)
-		}) as unknown as z.ZodObject<
-			{
-				node: z.ZodLiteral<Name>
-				input: z.ZodObject<
-					{
-						[K in keyof Input]: z.ZodUnion<
-							[Input[K], ...ReturnType<typeof compatableWith<Input[K], typeof definitions>>]
-						>
-					},
-					$strict
-				>
-			},
-			$strict
-		>
+		})
 	}
 
 	const definitions = Object.fromEntries(
 		Object.entries(nodes).map(([name, node]) => [
 			name,
 			{
-				definition: createDefinitions({ name, input: node.input }),
+				definition: createNodeDefinition({ name, input: node.input }),
 				output: node.output
 			}
 		])
 	) as {
-		[K in keyof Nodes]: {
-			definition: ReturnType<typeof createDefinitions<K extends string ? K : never, Nodes[K]['input']>>
-			output: Nodes[K]['output']
-		}
+		[K in keyof Nodes]: Definition<K & string, Nodes[K]['input'], Nodes>
 	}
 
 	const compatabilities = Object.fromEntries(
@@ -154,29 +162,14 @@ export function createChain<Nodes extends Record<string, GenericNode>>(nodes: No
 							key,
 							{
 								shape: shapeOf(arg),
-								zod: z.union([
-									arg,
-									...compatableWith({ shapeName: `${name}.${key}`, type: arg, definitions })
-								])
+								zod: z.union([...getNodeCompatabilities({ type: arg, definitions })])
 							}
 						]
 					})
 				)
 			]
 		})
-	) as {
-		[K in keyof Nodes]: {
-			[K2 in keyof Nodes[K]['input']]: {
-				shape: ShapeOf<Nodes[K]['input'][K2]>
-				zod: z.ZodUnion<
-					[
-						Nodes[K]['input'][K2],
-						...ReturnType<typeof compatableWith<Nodes[K]['input'][K2], typeof definitions>>
-					]
-				>
-			}
-		}
-	}
+	)
 
 	return {
 		definitions: Object.values(definitions).map(({ definition }) => definition) as {
